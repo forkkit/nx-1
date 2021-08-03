44fd4d3bc3a51f2d39104693ff194d258dc97767
Emily Xiong <xiongemi@gmail.com>
2021-08-02 21:00:22 -0400

    update versions

--- packages/react-native/src/utils/versions.ts
+++ packages/react-native/src/utils/versions.ts
@@ -1,7 +1,7 @@
 export const nxVersion = '*';
 
 export const reactVersion = '17.0.2';
-export const typesReactVersion = '17.0.6';
+export const typesReactVersion = '17.0.15';
 
 export const reactNativeVersion = '0.65.0-rc.3';
 export const typesReactNativeVersion = '0.64.12';
@@ -15,7 +15,7 @@ export const reactNativeCommunityCliAndroid = '6.0.0-rc.0';
 export const metroReactNativeBabelPresetVersion = '0.66.2';
 
 export const testingLibraryReactNativeVersion = '8.0.0-rc.0';
-export const testingLibraryJestNativeVersion = '4.0.1';
+export const testingLibraryJestNativeVersion = '4.0.2';
 
 export const jestReactNativeVersion = '18.0.0';
 
